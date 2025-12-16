263e359069b5fc806c079fc17fc0c537cc7a329e
XNoctra <hanabibadak11@gmail.com>
2025-12-16 16:52:40 +0700

    Update LogParser.php

--- src/Utils/LogParser.php
+++ src/Utils/LogParser.php
@@ -3,27 +3,24 @@ namespace LogAnlyzer\Utils;
 
 class LogParser
 {
-    /**
-     * Parse one line of access log
-     * Supports common Apache / Nginx log format
-     */
     public static function parseLine(string $line): ?array
     {
         /*
-         Example log:
+         Support Apache / Nginx combined log
+         Example:
          192.168.1.10 - - [16/Dec/2025:10:12:33 +0700] "POST /login HTTP/1.1" 200 532
         */
 
-        $pattern = '/^(\S+) .*? .*? "(GET|POST|PUT|DELETE|HEAD|OPTIONS) (\S+)/';
+        $pattern = '/^(\d+\.\d+\.\d+\.\d+).*?"([A-Z]+)\s+([^"]+?)\s+HTTP\/[0-9.]+"/';
 
-        if (!preg_match($pattern, $line, $matches)) {
+        if (!preg_match($pattern, $line, $m)) {
             return null;
         }
 
         return [
-            'ip'     => $matches[1],
-            'method' => $matches[2],
-            'path'   => $matches[3],
+            'ip'     => $m[1],
+            'method' => $m[2],
+            'path'   => $m[3],
         ];
     }
 }
